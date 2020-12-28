92ff5ecaef900ee56a1dbf5184c63a37fe580fe1
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-12-28 23:40:30 +0900

    TEST makeFakeData

---
+++ type/utils/index.ts
@@ -0,0 +1,5 @@
+import {getFIleNameAndNumber, FileNameAndNumber} from './getFileNameAndNumber'
+import {range} from './range'
+import {zip} from './zip'
+
+export {getFIleNameAndNumber, FileNameAndNumber, range, zip}
