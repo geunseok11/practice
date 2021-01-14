dfd1bf8fc6c89090346ddb5739e3a24e148ea717
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-01-14 23:45:37 +0900

    TEST react ts

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { getDataPromise } from './getDataPromise';
 import { IUser } from './IUser';
+import {Card} from './Card';
 
 const App: React.FC = () => {
+  const limit = 1
+  const [skip, setSkip] = useState(0)
   const [users, setUsers] = useState<IUser[]>([])
-  getDataPromise((receivedUsers:IUser[]) => setUsers([...users, ...receivedUsers]))(0,1)
+  const onClick = () => {
+    getDataPromise((receivedUsers:IUser[]) => {
+      setSkip(skip + limit)
+      setUsers([...users, ...receivedUsers])})(skip,limit)
+    }
+  useEffect(onClick,[])
   return(
-  <div className='App'>  plese open console window </div>
-  )
+  <div className='App'> {users.map((user:IUser,key:number) => (<Card click={onClick} user={user} key={key.toString()}/>))}
+  <p>{JSON.stringify(users)}</p>
+  </div>
+ )
 }
 
 export default App;
